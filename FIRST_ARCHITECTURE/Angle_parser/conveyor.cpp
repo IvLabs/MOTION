ae2df598e7f0b8cc42157f129a41b627cd17bbfb
surabhi96 <surabhiverma96@gmail.com>
2016-09-14 20:59:47 +0530

    Update conveyor.cpp

--- FIRST_ARCHITECTURE/Angle_parser/conveyor.cpp
+++ FIRST_ARCHITECTURE/Angle_parser/conveyor.cpp
@@ -1,31 +1,31 @@
 #include "ros/ros.h"
-#include "motion/angles_in.h"
-#include "herkulex.cpp"
+#include "motion/angles10.h"
+#include "Herkulex.h"
+#include "Herkulex.cpp"
 
 int motor[10];
-void chatterCallback(const motion::angles_in::ConstPtr& msg) //ang_msg object
+void chatterCallback(const motion::angles10::ConstPtr& msg) //ang_msg object
 {
   for (int num=0; num<10; num++)
    {
-     motor[num] = msg->angle_to_motor_in[num];
+     motor[num] = msg->angle2motor[num];
    }
 }
 /////////////////
 
-int motor_id[10]={4, 2, 10, 5, 7, 12, 8, 1, 11, 6};
-for (int i=0;i<10;i++)
-  HerkulexClass::torqueON(motor_id[i]);
-
-/////////////////
 
 int main(int argc, char **argv)
 {
-  
+  HerkulexClass hc;
+  int motor_id[10]={4, 2, 10, 5, 7, 12, 8, 1, 11, 6};
+  for (int i=0;i<10;i++)
+  hc.torqueON(motor_id[i]);
+
   ros::init(argc, argv, "communicator");
   ros::NodeHandle i;
-  //ros::NodeHandle o;
-  ros::Subscriber sub_ang = i.subscribe("parse_to_motor", 1000, chatterCallback);
-  //ros::Publisher pub_ang = o.advertise<motion::angles_out>("chatter", 1000);
+
+  ros::Subscriber sub_ang = i.subscribe("parse_to_motor", 10, chatterCallback);
+  
   ros::Rate loop_rate(33);
   while (ros::ok())
   {
@@ -34,13 +34,13 @@ int main(int argc, char **argv)
     
     for (int i=0; i<10; i++)
      {
-       HerkulexClass::moveOne(motor_id[i], motor[i], 22, 1);
+       hc.moveOne(motor_id[i], motor[i], 22, 1);
      }
     //pub_ang.publish(ang_msg_o);
     
     for (int i=0; i<10; i++)
      {
-       ROS_INFO("%f",motor[i]);
+       ROS_INFO("%d",motor[i]);
      }
 
     loop_rate.sleep();
