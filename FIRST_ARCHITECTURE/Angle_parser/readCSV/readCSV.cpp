2397431bc14042d1645498dbb278c0193613793c
Sakshi Kakde <kakdesakshi@gmail.com>
2016-09-14 14:38:48 +0530

    Update readCSV.cpp

--- FIRST_ARCHITECTURE/Angle_parser/readCSV/readCSV.cpp
+++ FIRST_ARCHITECTURE/Angle_parser/readCSV/readCSV.cpp
@@ -1,34 +1,33 @@
 //input the number of rows and columns of the csv sheet
+//written by:sakshi
+//13/9/16
 #include "readCSV.h"
 
 std::vector<std::string> readCSV::storeCSV2array(const char* const filename,int row,int col)
 {
 //file opening	
 std::ifstream infile(filename);
- std:: vector<std::string> data;
-  std::  string line;
+std:: vector<std::string> data;
+std::  string line;
 std::cout << "process started" << std::endl;
 if ( !infile.good() )
           {
-			std::cout << "error while opening file.";
-			exit (EXIT_FAILURE);
-			
+	    std::cout << "error while opening file.";
+	    exit (EXIT_FAILURE);			
           }
 
-    while (getline(infile, line))
-    {   
-		//std::cout <<"entered loop";
-        data.push_back(line);
-	}
-    infile.close();
+while (getline(infile, line))
+{  
+  //std::cout <<"entered loop";
+  data.push_back(line);
+}
+infile.close();
 
-  // for (int i = 0; i < data.size(); ++i)
-    //{
-      // std::cout << data[i] <<std::endl;
-     //} 
+// for (int i = 0; i < data.size(); ++i)
+//{
+// std::cout << data[i] <<std::endl;
+//} 
 //converting vector<std::string> to vector<int>
-//std::cout<<"size"<<data.at(1)[1];
-
-
 return data;  
 }
+
